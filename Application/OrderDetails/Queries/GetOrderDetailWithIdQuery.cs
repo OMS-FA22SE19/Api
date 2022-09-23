99e2a22df35cc8e6452ec4c412e83f128a6afa93
Phan Ngoc Hoang Long <longpnh2001@gmail.com>
2022-09-24 00:13:55 +0700

    OMS-25 | Api to get orderDetail and update status of dish

---
+++ Application/OrderDetails/Queries/GetOrderDetailWithIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Models;
+using Application.OrderDetails.Response;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.OrderDetails.Queries
+{
+    public sealed class GetOrderDetailWithIdQuery : IRequest<Response<DishDto>>
+    {
+        [Required]
+        public int Id { get; init; }
+    }
+
+    public sealed class GetOrderDetailWithIdQueryHandler : IRequestHandler<GetOrderDetailWithIdQuery, Response<DishDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetOrderDetailWithIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<DishDto>> Handle(GetOrderDetailWithIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _unitOfWork.OrderDetailRepository.GetAsync(e => e.Id == request.Id, $"{nameof(OrderDetail.Food)},{nameof(OrderDetail.Order)}");
+            var mappedResult = _mapper.Map<DishDto>(result);
+            return new Response<DishDto>(mappedResult);
+        }
+    }
+}
