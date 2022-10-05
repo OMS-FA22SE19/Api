1a08f2456ba6ef62c15b5c2fc33150c3d0cc8b17
Phan Ngoc Hoang Long <longpnh2001@gmail.com>
2022-10-06 01:31:55 +0700

    OMS-38 | Fix bug for create order

---
+++ Application/Foods/Response/MenuFoodDto.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Mappings;
+using Application.CourseTypes.Response;
+using Application.Types.Response;
+using AutoMapper;
+using Core.Entities;
+
+namespace Application.Foods.Response
+{
+    public sealed class MenuFoodDto : IMapFrom<Food>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Ingredient { get; set; }
+        public bool Available { get; set; } = true;
+        public string PictureUrl { get; set; }
+        public bool IsDeleted { get; set; }
+        public CourseTypeDto CourseType { get; set; }
+        public IList<TypeDto> Types { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Food, MenuFoodDto>().ReverseMap();
+        }
+    }
+}
