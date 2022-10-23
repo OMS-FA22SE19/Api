fbf5edd97dfd0c91c3071a372bec50573d999cac
QuangBG <quang23201@gmail.com>
2022-10-23 16:36:27 +0700

    test deploy

---
+++ Core/Events/CourseTypeEvent.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Events
+{
+    public class CourseTypeEvent : BaseEvent
+    {
+        public string message { get; set; }
+        public CourseTypeEvent(string message) { this.message = message; }
+    }
+}
