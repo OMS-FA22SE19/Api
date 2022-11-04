475099d44e722c2b53d9d575ef2fea00e8342ef8
QuangBG <quang23201@gmail.com>
2022-11-04 08:11:38 +0700

    add noti firebase

--- Application/Reservations/Events/CheckInReservationEvent.cs
+++ Application/Reservations/Events/CheckInReservationEvent.cs
@@ -5,6 +5,9 @@ using Core.Entities;
 using Domain.Common;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using FirebaseAdmin.Messaging;
 
 namespace Application.Orders.Events
 {
@@ -24,7 +27,26 @@ namespace Application.Orders.Events
         public async Task Handle(CheckInReservationEvent notification, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Reservation id {0} check in with table {1}", notification.ReservationId, String.Join(", ", notification.tableIds));
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("private_key.json")
+            });
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>() 
+                {
+                    { "myData", "1337" }
+                },
+                Topic = "all",
+                Notification = new Notification()
+                {
+                    Title = "Your Table have been checked in",
+                    Body = "Your Table have been checked in"
+                }
+            };
 
+            string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+            Console.WriteLine("success: " + response);
         }
     }
 }
